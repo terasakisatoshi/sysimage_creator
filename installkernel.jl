cb99329b7f4f2f0e9b12df951adccc691cbb3a2b
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-05-09 11:35:27 +0900

    specify path/to/sysimage explicitly

--- installkernel.jl
+++ installkernel.jl
@@ -1,3 +1,6 @@
 using IJulia
+using Libdl
+
 installkernel("Julia-trace", "--project=@.", "--trace-compile=traced_nb.jl")
-installkernel("Julia-sys", "--project=@.", "--sysimage=sys")
+sysimage = joinpath(@__DIR__, "sys" * "." * Libdl.dlext)
+installkernel("Julia-sys", "--project=@.", "--sysimage=$(sysimage)")
