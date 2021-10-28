4f1bd42ea79158daf4e64fd83029c51f00823502
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-10-28 16:07:51 +0900

    dispatch path/to/jupytext

--- benchmark.jl
+++ benchmark.jl
@@ -1,2 +1,8 @@
-@time run(`jupytext --to ipynb --execute testout_naive.jl`)
-@time run(`jupytext --to ipynb --execute testout_sys.jl`)
+jupytext = Sys.which("jupytext")
+
+if isnothing(jupytext)
+    jupytext = normpath(Conda.SCRIPTDIR, "jupytext") # e.g. ~/.julia/conda/3/bin/jupytext
+end
+
+@time run(`$jupytext --to ipynb --execute testout_naive.jl`)
+@time run(`$jupytext --to ipynb --execute testout_sys.jl`)
