45fd17c741bc23cb42bb26f4a07c10a859e32e82
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-10-28 17:01:58 +0900

    use CI

--- tracecompile.jl
+++ tracecompile.jl
@@ -1,5 +1,6 @@
 using StatsPlots
 using Plots
-
-include(joinpath(pkgdir(StatsPlots), "test", "runtests.jl"))
+ENV["CI"] = true
 try include(joinpath(pkgdir(Plots), "test", "runtests.jl")) catch end
+include(joinpath(pkgdir(StatsPlots), "test", "runtests.jl"))
+ENV["CI"] = false
