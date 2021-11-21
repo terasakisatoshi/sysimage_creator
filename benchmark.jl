ca57b856edf70fedddad92ebd1cb7b39302c9b63
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-11-21 19:20:00 +0900

    add testset wheth our sysimage actually improves latency

--- benchmark.jl
+++ benchmark.jl
@@ -1,8 +1,15 @@
-jupytext = Sys.which("jupytext")
+using Test
 
-if isnothing(jupytext)
-    jupytext = normpath(Conda.SCRIPTDIR, "jupytext") # e.g. ~/.julia/conda/3/bin/jupytext
+@testset "benchmark" begin 
+    jupytext = Sys.which("jupytext")
+
+    if isnothing(jupytext)
+        jupytext = normpath(Conda.SCRIPTDIR, "jupytext") # e.g. ~/.julia/conda/3/bin/jupytext
+    end
+
+    t_naive = @elapsed run(`$jupytext --to ipynb --execute testout_naive.jl`)
+    t_sys = @elapsed run(`$jupytext --to ipynb --execute testout_sys.jl`)
+    @show t_naive
+    @show t_sys
+    @test t_naive > t_sys
 end
-
-@time run(`$jupytext --to ipynb --execute testout_naive.jl`)
-@time run(`$jupytext --to ipynb --execute testout_sys.jl`)
