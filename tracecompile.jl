183c9010e1d5d018c3ceb62f5ca239d7d6c0edf6
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-12-29 18:45:25 +0900

    warn when running runtests.jl of Plots failed

--- tracecompile.jl
+++ tracecompile.jl
@@ -1,6 +1,10 @@
 using StatsPlots
 using Plots
 ENV["CI"] = true
-try include(joinpath(pkgdir(Plots), "test", "runtests.jl")) catch end
+try
+	include(joinpath(pkgdir(Plots), "test", "runtests.jl"))
+catch
+	@warn "runtests.jl for Plots failed"
+end
 include(joinpath(pkgdir(StatsPlots), "test", "runtests.jl"))
 ENV["CI"] = false
