cc07a95121e18ed02b0b77d15e750c3f947ea53b
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-12-29 17:56:14 +0900

    fix

--- build.jl
+++ build.jl
@@ -1,13 +1,15 @@
 using PackageCompiler
 using Libdl: dlext
 
+sysimage_path = joinpath(@__DIR__, "v$(major).$(minor)", "sys" * "." * Libdl.dlext)
+
 create_sysimage(
     [:StatsPlots, :Plots, :DataFrames], 
     precompile_statements_file=[
         "traced_runtests.jl", 
         "traced_nb.jl"
     ],
-    sysimage_path=joinpath(@__DIR__, "sys.$(dlext)"),
+    sysimage_path=sysimage_path,
     cpu_target = PackageCompiler.default_app_cpu_target()
 )
 
--- installkernel.jl
+++ installkernel.jl
@@ -7,19 +7,19 @@ installkernel("Julia-trace", "--project=@.", "--trace-compile=traced_nb.jl")
 major = VERSION.major
 minor = VERSION.minor
 
-sysimage = joinpath(@__DIR__, "v$(major).$(minor)", "sys" * "." * Libdl.dlext)
-mkpath(sysimage)
+sysimage_path = joinpath(@__DIR__, "v$(major).$(minor)", "sys" * "." * Libdl.dlext)
+mkpath(dirname(sysimage))
 
 installkernel(
     "Julia-sys",
-    "--project=@.", "--sysimage=$(sysimage)"
+    "--project=@.", "--sysimage=$(sysimage_path)"
 )
 
 nthreads = Sys.CPU_THREADS
 
 installkernel(
     "julia-sys-$(nthreads)-threads",
-    "--project=@.", "--sysimage=$(sysimage)",
+    "--project=@.", "--sysimage=$(sysimage_path)",
     env=Dict(
         "JULIA_NUM_THREADS"=>"$(nthreads)",
     ),
